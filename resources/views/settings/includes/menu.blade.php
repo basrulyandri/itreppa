514643fccc06721f8e75a61afbd63ae1cffbde28
basrulyandri <digicrea08@gmail.com>
2018-10-23 17:54:28 +0700

    addendum aja

--- resources/views/settings/includes/menu.blade.php
+++ resources/views/settings/includes/menu.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<div class="col-md-12">
 	    <div id="nestable-menu">
-	        <a href="{{route('setting.general')}}" class="btn btn-white btn-sm">General</a>	        	        
+	        <a href="{{route('setting.general')}}" class="btn btn-white btn-sm">General</a>       
 	    </div>	    
     </div>
 </div>
